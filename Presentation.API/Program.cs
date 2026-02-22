d169f7d2faa33aa0ea38eadc9f8a44e40b4d3d33
FredricDomvall <Fredric.domvall@outlook.com>
2026-02-22 15:43:33 +0100

    cors and health endpoint configured in program.cs

--- Presentation.API/Program.cs
+++ Presentation.API/Program.cs
@@ -1,12 +1,21 @@
+using Application;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddCors();
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
+
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.MapGet("/api/health", () => Results.Ok(new { status = "ok" }));
 
 app.Run();
