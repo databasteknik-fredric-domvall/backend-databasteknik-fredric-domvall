1b129bb51cc87f58b620df723259414ef8c6c447
FredricDomvall <Fredric.domvall@outlook.com>
2026-02-22 15:13:22 +0100

    Infrastructure dependencyinjection created

---
+++ Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureDependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<CourseOnlineDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        return services;
+    }
+}
