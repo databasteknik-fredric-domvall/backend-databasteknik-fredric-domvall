00bb1226ad76dcef1a7f4a141a1a6bf89e4d1ced
FredricDomvall <Fredric.domvall@outlook.com>
2026-03-01 22:35:58 +0100

    student contracts added

---
+++ Presentation.API/Contracts/Students/StudentContracts.cs
@@ -0,0 +1,7 @@
+﻿namespace Presentation.API.Contracts.Students;
+
+public sealed record StudentResponse(Guid Id, string FirstName, string LastName, string Email);
+
+public sealed record CreateStudentRequest(string FirstName, string LastName, string Email);
+
+public sealed record UpdateStudentRequest(string FirstName, string LastName, string Email);
