204c214ed908b90b04ce2f6f86242d66c82097b8
FredricDomvall <Fredric.domvall@outlook.com>
2026-03-01 22:09:49 +0100

    course contracts added

---
+++ Presentation.API/Contracts/Courses/CourseContracts.cs
@@ -0,0 +1,7 @@
+﻿namespace Presentation.API.Contracts.Courses;
+
+public sealed record CourseResponse(Guid Id, string Title, string Description);
+
+public sealed record CreateCourseRequest(string Title, string Description);
+
+public sealed record UpdateCourseRequest(string Title, string Description);
